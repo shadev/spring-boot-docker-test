ce47ddc87aee416492da5c72be44770857ee104b
Stefan Hausner <stefan.hausner@comsysto.com>
2016-10-07 15:55:32 +0200

    Push new version

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World - IntegrityNext";
+        return "Hello Docker World - IntegrityNext gen 2";
     }
 
     public static void main(String[] args) {
